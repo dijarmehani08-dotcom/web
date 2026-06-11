3000c385df8d4c52811d09df10606c8aefd415bd
Dijar Mehani <dijar.mehani08@gmail.com>
2026-06-11 17:48:12 +0200

    lesson 26

---
+++ modul 26/js/main.js
@@ -0,0 +1,58 @@
+console.log("hello from Dijar babaa")
+
+var x=5;
+
+var y=28+43;
+console.log(y);
+
+
+var f=6%3;
+console.log(f);
+
+var c=99;
+c++;
+console.log(c);
+
+var c=99;
+c--;
+console.log(c);
+
+var q=4;
+var p=q**2;
+console.log(p);
+
+var b=6
+var a=6
+console.log(a==b);
+
+var b=6
+var a=7
+console.log(a==b);
+
+console.log("--------");
+
+var b=6
+var a="6"
+console.log(a==b);
+
+var b=6
+var a="6"
+console.log(a===b);
+
+var b=6
+var a=7
+console.log(a<b);
+
+var b=6
+var a=7
+console.log(a>b);
+
+var b=6
+var a=6
+console.log(a!=b);
+
+var name="dijar";
+var lastname="mehani";
+var fullname= name +" " + lastname;
+console.log(fullname);
+
